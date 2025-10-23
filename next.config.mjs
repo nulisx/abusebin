7a42204bfbd92370ed11df6d362cb126cf80bc3c
nulisx <edrmebitch@fbi.one>
2025-10-23 19:43:32 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,13 @@ const nextConfig = {
     unoptimized: true,
   },
   output: 'standalone',
+  webpack(config) {
+    config.optimization.splitChunks = {
+      chunks: 'all',
+      maxSize: 25000000,
+    };
+    return config;
+  },
 }
 
 export default nextConfig
