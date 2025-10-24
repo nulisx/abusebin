8974f0a2e946d045a7ff93bf68abb20f6941b41d
nulisx <edrmebitch@fbi.one>
2025-10-23 21:29:55 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   images: { unoptimized: true },
-  output: 'standalone',
+  output: 'export',
   webpack(config) {
     config.optimization.splitChunks = {
       chunks: 'all',
