58f8e0f8d8e1718a14d6bc1ea7c850741e860e3e
nulisx <edrmebitch@fbi.one>
2025-10-26 13:29:41 -0400

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  images: { unoptimized: true },
-  experimental: { esmExternals: true },
-  webpack(config) {
-    config.optimization.splitChunks = {
-      chunks: 'all',
-      maxSize: 25000000,
-    }
-    return config
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
   },
 }
 
